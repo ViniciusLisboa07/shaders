230fc755e1ed72c9a470b236c3da2c9d79d3f1a6
ViniciusLisboa07 <viniciuslisboa1001@gmail.com>
2023-12-28 23:12:55 -0300

    feat: :art: the rose that came to bloom

--- sketch.js
+++ sketch.js
@@ -1,10 +1,25 @@
+let angle = 0;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(700, 700);
+  rectMode(CENTER);
+  angleMode(DEGREES);
+  strokeWeight(50);
+  stroke(255);
 }
 
 function draw() {
-  background(100);
-  ellipse(200, 200, 50, 50);
+  background(255);
 
-  rect(100, 100, 50, 50)
+  translate(width/2, height/2);
+  
+  for (var i = 0; i < 100; i++) {
+    fill(i*100, i+50, i*2);
+    rotate(angle);
+    rect(0, 0, 500, 500);
+    scale(0.8)
+    rotate(angle);
+  }
+
+  angle += 0.2;
 }
